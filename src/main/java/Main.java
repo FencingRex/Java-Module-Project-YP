a2a94c0bf18e255a8aedaeb240c386629349c0b9
FencingRex <phenix3legio@gmail.com>
2025-05-26 09:36:51 +0300

    Проект 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Main {
+    String winner="";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int autoSpeed = -1;
         String autoName = "";
         String finalWinner = "";
         Auto[] autoList = new Auto[3];
+        Race raceCls = new Race();
         System.out.println("Welcome to Le-Mans 24");
         for (int i = 0; i < autoList.length;i++) {
             System.out.println("Введите название автомобиля #" + (i + 1) + ":");
@@ -13,14 +15,20 @@ public class Main {
 
             while (true){
                 System.out.println("Укажите скорость автомобиля #" + (i + 1) + ": ");
-                autoSpeed = scanner.nextInt();
-                if (autoSpeed >0 && autoSpeed <=250){
-                    break;
+                if (scanner.hasNextInt()) {
+                    autoSpeed = scanner.nextInt();
+                    if (autoSpeed > 0 && autoSpeed <= 250) {
+                        break;
+                    } else {
+                        System.out.println("Некорректное значение скорости, введите скорость в диапазоне от 0 до 250");
+                    }
+                } else{
+                    System.out.println("Скорость должна быть указв в виде целого числа");
+                    scanner.next();
                 }
             }
             autoList[i] = new Auto(autoName,autoSpeed);
-            Race raceCls = new Race();
-            finalWinner = raceCls.setRaceWinner(autoName,autoSpeed);
+            finalWinner = raceCls.setRaceWinner(autoName,autoSpeed,finalWinner);
         }
         /*for (int i=0; i <3; i++){
             System.out.println("Атомобиль "+autoList[i].name +" движется со скоростью "+ autoList[i].speed +" км/ч");
@@ -39,16 +47,16 @@ class Auto {
     }
 }
 class Race{
-    String winner="";
+    //String winner="";
     int pathLength = 0;
 
-    public String setRaceWinner(String name, int speed){
-        if (winner.equals("")){
+    public String setRaceWinner(String name, int speed,String winner){
+        if (winner.isEmpty()){   //winner.equals("")
             pathLength = 24 * speed;
             winner = name;
         } else {
             int newPathLeng = 24 * speed;
-            if (newPathLeng>pathLength){
+            if (newPathLeng > pathLength){
                 pathLength = newPathLeng;
                 winner = name;
             }
